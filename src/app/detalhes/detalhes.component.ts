e70d513d086917d7b71cba01d6e7c00efec78a2a
EnzoPravatto <137100400+EnzoPravatto@users.noreply.github.com>
2023-08-31 23:32:52 -0300

    Update detalhes.component.ts

--- src/app/detalhes/detalhes.component.ts
+++ src/app/detalhes/detalhes.component.ts
@@ -11,7 +11,7 @@ export class DetalhesComponent implements OnInit {
   imovelId: string;
 
   titulo: string = 'Motos';
-  foto: string = 'https://img.wtvideo.com/images/original/587.jpg';
+  foto: string = '#';
 
   description: string = 'Motociclo ou motocicleta (também conhecida simplesmente por motoca, moto ou ainda mota), é um veículo motorizado de duas rodas e tracção traseira e salvo raras excepções, capaz de desenvolver velocidade de cruzeiro com segurança e conforto. É um meio de transporte bastante utilizado devido ao mais baixo consumo de combustível e por ter um preço mais acessível que a maioria dos automóveis. Entretanto, há motos que consomem mais combustível do que muitos automóveis, variando, entre outros fatores, com a cilindrada do motor. Além de um meio de transporte, a motocicleta pode ser usada por áreas como desportos, lazer, utilitária e veículo militar.';
   description2: string = '';
